1d1220d14a068f11934d115e545b7a326d6f06e5
Sphehh <sphesihlendlovu266@gmail.com>
2026-05-29 21:44:22 +0200

    Update adzuna-proxy.js

--- netlify/functions/adzuna-proxy.js
+++ netlify/functions/adzuna-proxy.js
@@ -1,1 +1,18 @@
 
+exports.handler = async () => {
+  const APP_ID = process.env.ADZUNA_APP_ID;
+  const APP_KEY = process.env.ADZUNA_APP_KEY;
+  const url = `https://api.adzuna.com/v1/api/jobs/za/search/1?app_id=${APP_ID}&app_key=${APP_KEY}&results_per_page=60`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'public, max-age=900'   // CDN cache 15 min
+    },
+    body: JSON.stringify(data)
+  };
+};
